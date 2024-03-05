8685f1c798bd06afa60c034754e189631079f788
Drew Miller <drew.miller@mrjaffesclass.com>
2024-03-05 12:49:29 -0800

    init

---
+++ src/main/java/com/mycompany/tictactoe/TicTacToe.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tictactoe;
+
+/**
+ *
+ * @author student
+ */
+public class TicTacToe {
+    public static void main(String[] args) {
+        TicTacToe ttt = new TicTacToe();
+    }
+    
+    public TicTacToe() {
+        View view = new View();
+        view.setVisible(true);
+    }
+}
